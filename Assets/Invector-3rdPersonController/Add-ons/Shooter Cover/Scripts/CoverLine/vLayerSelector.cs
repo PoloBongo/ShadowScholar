270011fa63af9865082b14fffe19ab9c2a498dbe
Evan Choquet <echoquet@gaming.tech>
2024-06-19 15:13:01 +0200

    [UPDATE] assets reorganization

--- Assets/Invector-3rdPersonController/Add-ons/Shooter Cover/Scripts/CoverLine/vLayerSelector.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-[System.AttributeUsage(System.AttributeTargets.Field)]
-public class vLayerSelectorAttribute : PropertyAttribute
-{
-    
-}

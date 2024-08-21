675fc7b3437d58ad72f7fe928e08eea2c6021290
Evan Choquet <echoquet@gaming.tech>
2024-08-22 00:57:46 +0200

    [UPDATE] Test Setup Map Zone 18

--- Assets/Editor/CreateLODGroup.cs
+++ Assets/Editor/CreateLODGroup.cs
@@ -1,4 +1,4 @@
-/*using System.Collections.Generic;
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 using UnityMeshSimplifier;
@@ -70,55 +70,52 @@ public class CreateLODGroup : MonoBehaviour
         // Assigner les renderers aux LODs en fonction de leur taille et de leur tag/nom
         for (int i = 0; i < renderersList.Count; i++)
         {
-            float normalizedSize = (rendererSizes[i] - minSize) / (maxSize - minSize);
-            GameObject rendererObject = renderersList[i].gameObject;
+            if (renderersList[i].gameObject.GetComponent<LODGroup>() == null && !renderersList[i].gameObject.name.Contains("LOD"))
+            {
 
-            // Vérifier si l'objet est dans un dossier "Exterieur" ou "Interior"
-            bool isExteriorObj = IsInExteriorFolder(rendererObject.transform);
-            bool isInteriorObj = IsInInteriorFolder(rendererObject.transform);
+                float normalizedSize = (rendererSizes[i] - minSize) / (maxSize - minSize);
+                GameObject rendererObject = renderersList[i].gameObject;
 
-            if (isExteriorObj)
-            {
-                lodRenderers[0].Add(renderersList[i]);
-                LODSimplifyMeshIteratively(1, 0.5f, renderersList[i], rendererObject, lodRenderers);
-                LODSimplifyMeshIteratively(2, 0.2f, renderersList[i], rendererObject, lodRenderers);
-            }
-            else if (isInteriorObj)
-            {
-                // Ajouter uniquement au premier niveau de LOD
-                lodRenderers[0].Add(renderersList[i]);
-            }
-            else
-            {
-                if (normalizedSize < 0.1f)
+                // Vérifier si l'objet est dans un dossier "Exterieur" ou "Interior"
+                bool isExteriorObj = IsInExteriorFolder(rendererObject.transform);
+                bool isInteriorObj = IsInInteriorFolder(rendererObject.transform);
+
+                if (isExteriorObj)
                 {
                     lodRenderers[0].Add(renderersList[i]);
+                    lodRenderers[1].Add(renderersList[i]);
+                    lodRenderers[2].Add(renderersList[i]);
                 }
-                else if (normalizedSize < 0.3f)
+                else if (isInteriorObj)
                 {
                     lodRenderers[0].Add(renderersList[i]);
-                    LODSimplifyMeshIteratively(1, 0.5f, renderersList[i], rendererObject, lodRenderers);
                 }
                 else
                 {
-                    lodRenderers[0].Add(renderersList[i]);
-                    LODSimplifyMeshIteratively(1, 0.5f, renderersList[i], rendererObject, lodRenderers);
-                    LODSimplifyMeshIteratively(2, 0.2f, renderersList[i], rendererObject, lodRenderers);
+                    if (normalizedSize < 0.1f)
+                    {
+                        lodRenderers[0].Add(renderersList[i]);
+                    }
+                    else if (normalizedSize < 0.3f)
+                    {
+                        lodRenderers[0].Add(renderersList[i]);
+                        lodRenderers[1].Add(renderersList[i]);
+                    }
+                    else
+                    {
+                        lodRenderers[0].Add(renderersList[i]);
+                        lodRenderers[1].Add(renderersList[i]);
+                        lodRenderers[2].Add(renderersList[i]);
+                    }
                 }
             }
         }
 
-        // Créer les niveaux de LOD avec des pourcentages ajustés dynamiquement
-        float[] lodPercentages = { 0.5f, 0.25f, 0.1f }; // Ajuster les pourcentages pour LOD 0, LOD 1 et LOD 2
-        float[] lodScreenRelativeHeights = new float[lodCount];
-        for (int i = 0; i < lodCount; i++)
-        {
-            lodScreenRelativeHeights[i] = lodPercentages[i] / maxObjectSize;
-        }
+        float[] lodPercentages = { 0.45f, 0.2f, 0.02f }; // Ajuster les pourcentages pour LOD 0, LOD 1 et LOD 2
 
         for (int i = 0; i < lodCount; i++)
         {
-            lods[i] = new LOD(lodScreenRelativeHeights[i], lodRenderers[i].ToArray());
+            lods[i] = new LOD(lodPercentages[i], lodRenderers[i].ToArray());
         }
 
         // Appliquer les niveaux de LOD au LOD Group
@@ -155,90 +152,7 @@ public class CreateLODGroup : MonoBehaviour
             return false;
         }
 
-        void LODSimplifyMeshIteratively(int lod, float quality, Renderer renderer, GameObject rendererObject, List<Renderer>[] lodRenderers)
-        {
-            MeshFilter meshFilter = rendererObject.GetComponent<MeshFilter>();
-
-            if (meshFilter != null)
-            {
-                Mesh originalMesh = meshFilter.sharedMesh;
-                Debug.Log("Original Mesh Triangle Count: " + originalMesh.triangles.Length / 3);
-
-                // Assurez-vous que la qualité n'est pas trop basse
-                quality = Mathf.Clamp(quality, 0.1f, 1.0f);
-
-                Mesh simplifiedMesh = SimplifyMeshIteratively(originalMesh, quality);
-                Debug.Log("Simplified Mesh Triangle Count: " + simplifiedMesh.triangles.Length / 3);
-
-                GameObject lodObject = new GameObject(rendererObject.name + "_LOD" + lod);
-                lodObject.transform.SetParent(rendererObject.transform);
-
-                lodObject.transform.localPosition = Vector3.zero;
-                lodObject.transform.localRotation = Quaternion.identity;
-                lodObject.transform.localScale = Vector3.one;
-
-                MeshFilter lodMeshFilter = lodObject.AddComponent<MeshFilter>();
-                MeshRenderer lodMeshRenderer = lodObject.AddComponent<MeshRenderer>();
-                lodMeshRenderer.sharedMaterials = renderer.sharedMaterials;
-                lodMeshFilter.sharedMesh = simplifiedMesh;
-
-                lodRenderers[lod].Add(lodMeshRenderer);
-            }
-            else
-            {
-                lodRenderers[lod].Add(renderer);
-            }
-        }
-
-        Mesh SimplifyMeshIteratively(Mesh originalMesh, float targetQuality)
-        {
-            MeshSimplifier meshSimplifier = new MeshSimplifier();
-            meshSimplifier.Initialize(originalMesh);
-
-            // Configurer les options de simplification
-            SimplificationOptions options = new SimplificationOptions
-            {
-                PreserveBorderEdges = true,
-                PreserveUVSeamEdges = false,
-                PreserveUVFoldoverEdges = false,
-                PreserveSurfaceCurvature = false,
-                EnableSmartLink = true,
-                VertexLinkDistance = 0.001f,
-                MaxIterationCount = 50,  // Réduit le nombre d'itérations
-                Agressiveness = 2.0,     // Réduit l'agressivité
-                ManualUVComponentCount = false
-            };
-
-            meshSimplifier.SimplificationOptions = options;
-
-            // Simplifier par étapes
-            float currentQuality = 1.0f;
-            while (currentQuality > targetQuality)
-            {
-                float nextQuality = Mathf.Max(currentQuality - 0.05f, targetQuality);
-                try
-                {
-                    meshSimplifier.SimplifyMesh(nextQuality);
-                }
-                catch (System.IndexOutOfRangeException ex)
-                {
-                    Debug.LogWarning("IndexOutOfRangeException caught during mesh simplification. Adjusting the quality step.");
-                    break;
-                }
-                currentQuality = nextQuality;
-            }
-
-            Mesh simplifiedMesh = meshSimplifier.ToMesh();
-
-            // Recalculer les données du maillage
-            simplifiedMesh.RecalculateBounds();
-            simplifiedMesh.RecalculateNormals();
-            simplifiedMesh.RecalculateTangents();
-
-            return simplifiedMesh;
-        }
-
     }
 }
-*/
+
 
